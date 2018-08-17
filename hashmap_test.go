7aeb8ac2e5d5f87de057b64e82ba77e0bf542b0e
Jeffrey D <verygoodsoftwarenotvirus@users.noreply.github.com>
2018-08-17 18:20:20 -0500

    A couple more tests on the pile!

--- hashmap_test.go
+++ hashmap_test.go
@@ -1,12 +1,15 @@
 package hashmap
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/nacl/sign"
 )
 
 const (
@@ -27,10 +30,10 @@ var (
 	}
 )
 
-func buildExamplePayload() *Payload {
-	p := &Payload{}
-	json.Unmarshal([]byte(exampleValidPayload), &p)
-	return p
+// Key returns a randomly generated ed25519 private key in bytes
+func buildInvalidKey() string {
+	_, privKey, _ := sign.GenerateKey(rand.Reader)
+	return base64.StdEncoding.EncodeToString(privKey[31:])
 }
 
 type errorReader struct{}
@@ -45,22 +48,16 @@ func TestNewPayloadFromReader(T *testing.T) {
 	T.Run("normal operation", func(t *testing.T) {
 		t.Parallel()
 
-		exampleReader := strings.NewReader(exampleValidPayload)
-		expected := &Payload{
-			Data:      "eyJtZXNzYWdlIjoiZXlKamIyNTBaVzUwSWpvaWFHVnNiRzhzSUhkdmNteGtMaUJVYUdseklHbHpJR1JoZEdFZ2MzUnZjbVZrSUdsdUlFaGhjMmhOWVhBdUluMD0iLCJ0aW1lc3RhbXAiOjE1MzQ0NzcyMzAsInR0bCI6ODY0MDAsInNpZ01ldGhvZCI6Im5hY2wtc2lnbi1lZDI1NTE5IiwidmVyc2lvbiI6IjAuMC4xIn0=",
-			Signature: "xYUd7E99i5yYVg1IfsgpjpmTL1R5lqB1B2R+TqpibGuytHQ4p1oK4HKeYDJkBP3u1F/132LtOuLmOqqriqfMAA==",
-			PublicKey: "9HYoFN26rgJ8NSEGHshlmt+OwKmJrMAWJM1cAKyGbeo=",
-		}
-		actual, err := NewPayloadFromReader(exampleReader)
-
+		actual, err := NewPayloadFromReader(strings.NewReader(exampleValidPayload))
 		assert.NoError(t, err)
-		assert.Equal(t, expected, actual)
+		assert.Equal(t, examplePayload, actual)
 	})
 
 	T.Run("with erroneous input", func(t *testing.T) {
 		t.Parallel()
 
-		_, err := NewPayloadFromReader(&errorReader{})
+		shouldBeNil, err := NewPayloadFromReader(&errorReader{})
+		assert.Nil(t, shouldBeNil)
 		assert.Error(t, err)
 	})
 
@@ -68,7 +65,8 @@ func TestNewPayloadFromReader(T *testing.T) {
 		t.Parallel()
 
 		exampleReader := strings.NewReader("not json lol")
-		_, err := NewPayloadFromReader(exampleReader)
+		shouldBeNil, err := NewPayloadFromReader(exampleReader)
+		assert.Nil(t, shouldBeNil)
 		assert.Error(t, err)
 	})
 
@@ -83,7 +81,8 @@ func TestNewPayloadFromReader(T *testing.T) {
 		`
 		exampleReader := strings.NewReader(examplePayload)
 
-		_, err := NewPayloadFromReader(exampleReader)
+		shouldBeNil, err := NewPayloadFromReader(exampleReader)
+		assert.Nil(t, shouldBeNil)
 		assert.Error(t, err)
 	})
 }
@@ -101,71 +100,96 @@ func TestNewValidator(T *testing.T) {
 	T.Run("with pubkey failure", func(t *testing.T) {
 		t.Parallel()
 
-		expected := &Payload{
+		example := &Payload{
 			Data:      "eyJtZXNzYWdlIjoiZXlKamIyNTBaVzUwSWpvaWFHVnNiRzhzSUhkdmNteGtMaUJVYUdseklHbHpJR1JoZEdFZ2MzUnZjbVZrSUdsdUlFaGhjMmhOWVhBdUluMD0iLCJ0aW1lc3RhbXAiOjE1MzQ0NzcyMzAsInR0bCI6ODY0MDAsInNpZ01ldGhvZCI6Im5hY2wtc2lnbi1lZDI1NTE5IiwidmVyc2lvbiI6IjAuMC4xIn0=",
 			Signature: "xYUd7E99i5yYVg1IfsgpjpmTL1R5lqB1B2R+TqpibGuytHQ4p1oK4HKeYDJkBP3u1F/132LtOuLmOqqriqfMAA==",
 			PublicKey: "nope lol",
 		}
-		_, err := expected.NewValidator()
-		assert.Error(T, err)
+		shouldBeNil, err := example.NewValidator()
+		assert.Nil(t, shouldBeNil)
+		assert.Error(t, err)
 	})
 	T.Run("with signature failure", func(t *testing.T) {
 		t.Parallel()
 
-		expected := &Payload{
+		example := &Payload{
 			Data:      "eyJtZXNzYWdlIjoiZXlKamIyNTBaVzUwSWpvaWFHVnNiRzhzSUhkdmNteGtMaUJVYUdseklHbHpJR1JoZEdFZ2MzUnZjbVZrSUdsdUlFaGhjMmhOWVhBdUluMD0iLCJ0aW1lc3RhbXAiOjE1MzQ0NzcyMzAsInR0bCI6ODY0MDAsInNpZ01ldGhvZCI6Im5hY2wtc2lnbi1lZDI1NTE5IiwidmVyc2lvbiI6IjAuMC4xIn0=",
 			Signature: "nope lol",
 			PublicKey: "9HYoFN26rgJ8NSEGHshlmt+OwKmJrMAWJM1cAKyGbeo=",
 		}
-		_, err := expected.NewValidator()
-		assert.Error(T, err)
+		shouldBeNil, err := example.NewValidator()
+		assert.Nil(t, shouldBeNil)
+		assert.Error(t, err)
 	})
 	T.Run("with data failure", func(t *testing.T) {
 		t.Parallel()
 
-		expected := &Payload{
+		example := &Payload{
 			Data:      "🙉🙈🙊",
 			Signature: "xYUd7E99i5yYVg1IfsgpjpmTL1R5lqB1B2R+TqpibGuytHQ4p1oK4HKeYDJkBP3u1F/132LtOuLmOqqriqfMAA==",
 			PublicKey: "9HYoFN26rgJ8NSEGHshlmt+OwKmJrMAWJM1cAKyGbeo=",
 		}
-		_, err := expected.NewValidator()
-		assert.Error(T, err)
+		shouldBeNil, err := example.NewValidator()
+		assert.Nil(t, shouldBeNil)
+		assert.Error(t, err)
 	})
 	T.Run("with GetData failure", func(t *testing.T) {
 		t.Parallel()
 
-		expected := &Payload{
+		example := &Payload{
 			Data:      "8J+ZifCfmYjwn5mK", // base64 encoded monkeys
 			Signature: "xYUd7E99i5yYVg1IfsgpjpmTL1R5lqB1B2R+TqpibGuytHQ4p1oK4HKeYDJkBP3u1F/132LtOuLmOqqriqfMAA==",
 			PublicKey: "9HYoFN26rgJ8NSEGHshlmt+OwKmJrMAWJM1cAKyGbeo=",
 		}
-		_, err := expected.NewValidator()
-		assert.Error(T, err)
+		shouldBeNil, err := example.NewValidator()
+		assert.Nil(t, shouldBeNil)
+		assert.Error(t, err)
+	})
+	T.Run("with invalid signature method", func(t *testing.T) {
+		t.Parallel()
+
+		example := &Payload{
+			Data:      "eyJtZXNzYWdlIjoiZXlKamIyNTBaVzUwSWpvaWFHVnNiRzhzSUhkdmNteGtMaUJVYUdseklHbHpJR1JoZEdFZ2MzUnZjbVZrSUdsdUlFaGhjMmhOWVhBdUluMD0iLCJ0aW1lc3RhbXAiOjE1MzQ0NzcyMzAsInR0bCI6ODY0MDAsInNpZ01ldGhvZCI6InNvbWV0aGluJyBlbHNlIiwidmVyc2lvbiI6IjAuMC4xIn0=",
+			Signature: "xYUd7E99i5yYVg1IfsgpjpmTL1R5lqB1B2R+TqpibGuytHQ4p1oK4HKeYDJkBP3u1F/132LtOuLmOqqriqfMAA==",
+			PublicKey: "9HYoFN26rgJ8NSEGHshlmt+OwKmJrMAWJM1cAKyGbeo=",
+		}
+
+		shouldBeNil, err := example.NewValidator()
+		assert.Nil(t, shouldBeNil)
+		assert.Error(t, err)
+	})
+	T.Run("with too long a public key", func(t *testing.T) {
+		t.Parallel()
+
+		example := &Payload{
+			Data:      "eyJtZXNzYWdlIjoiZXlKamIyNTBaVzUwSWpvaWFHVnNiRzhzSUhkdmNteGtMaUJVYUdseklHbHpJR1JoZEdFZ2MzUnZjbVZrSUdsdUlFaGhjMmhOWVhBdUluMD0iLCJ0aW1lc3RhbXAiOjE1MzQ0NzcyMzAsInR0bCI6ODY0MDAsInNpZ01ldGhvZCI6Im5hY2wtc2lnbi1lZDI1NTE5IiwidmVyc2lvbiI6IjAuMC4xIn0=",
+			Signature: "xYUd7E99i5yYVg1IfsgpjpmTL1R5lqB1B2R+TqpibGuytHQ4p1oK4HKeYDJkBP3u1F/132LtOuLmOqqriqfMAA==",
+			PublicKey: buildInvalidKey(),
+		}
+
+		shouldBeNil, err := example.NewValidator()
+		assert.Nil(t, shouldBeNil)
+		assert.Error(t, err)
 	})
 }
 
-// // NOTE: I wrote this and then wondered if I was actually testing anything.
-// func TestPayload_PubKeyBytes(T *testing.T) {
-// 	T.Parallel()
+func TestPayload_PubKeyBytes(T *testing.T) {
+	T.Parallel()
 
-// 	T.Run("normal operation", func(t *testing.T) {
-// 		x := buildExamplePayload()
+	T.Run("normal operation", func(t *testing.T) {
+		actual, err := examplePayload.PubKeyBytes()
 
-// 		expected := []byte{0xf4, 0x76, 0x28, 0x14, 0xdd, 0xba, 0xae, 0x2, 0x7c, 0x35, 0x21, 0x6, 0x1e, 0xc8, 0x65, 0x9a, 0xdf, 0x8e, 0xc0, 0xa9, 0x89, 0xac, 0xc0, 0x16, 0x24, 0xcd, 0x5c, 0x0, 0xac, 0x86, 0x6d, 0xea}
-// 		actual, err := x.PubKeyBytes()
-
-// 		assert.NoError(t, err)
-// 		assert.Equal(t, expected, actual)
-// 	})
-// }
+		assert.NoError(t, err)
+		assert.NotEmpty(t, actual)
+	})
+}
 
 func TestPayload_Verify(T *testing.T) {
 	T.Parallel()
 
 	T.Run("normal operation", func(t *testing.T) {
 		t.Parallel()
-		p := buildExamplePayload()
-		assert.NoError(t, p.Verify())
+		assert.NoError(t, examplePayload.Verify())
 	})
 
 	T.Run("with erroneous input", func(t *testing.T) {
@@ -181,5 +205,26 @@ func TestPayload_Verify(T *testing.T) {
 		json.Unmarshal([]byte(badPayload), &p)
 
 		assert.Error(t, p.Verify())
+	})
+}
+
+func TestPayload_GetData(T *testing.T) {
+	T.Parallel()
+
+	T.Run("normal operation", func(t *testing.T) {
+		actual, err := examplePayload.DataBytes()
+		assert.Empty(t, actual)
+		assert.NoError(t, err)
+	})
+
+	T.Run("with error decoding data", func(t *testing.T) {
+		example := &Payload{
+			Data:      "🙉🙈🙊",
+			Signature: "xYUd7E99i5yYVg1IfsgpjpmTL1R5lqB1B2R+TqpibGuytHQ4p1oK4HKeYDJkBP3u1F/132LtOuLmOqqriqfMAA==",
+			PublicKey: "9HYoFN26rgJ8NSEGHshlmt+OwKmJrMAWJM1cAKyGbeo=",
+		}
+		actual, err := example.GetData()
+		assert.Nil(t, actual)
+		assert.Error(t, err)
 	})
 }
