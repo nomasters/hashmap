ae2e9bbb890745616581f6360b3088bdfe093dd2
Nathan Toups <n@rjrbt.io>
2018-08-23 23:06:41 -0600

    updated config file description

--- hashmap/cmd/root.go
+++ hashmap/cmd/root.go
@@ -57,7 +57,7 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hashmap.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is in the current directory: ./hashmap.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
